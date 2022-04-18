50c21cff0546021e00483813b2cc296c9525b527
engrasel <contact.engrasel@gmail.com>
2022-04-19 01:56:38 +0600

    2nd commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,24 @@
 const toggleForm = () => {
-  const container = document.querySelector(".container");
-  container.classList.toggle("active");
+  const signinBx_container = document.querySelector(".signinBx_container");
+  signinBx_container.classList.toggle("active");
 };
+
+//  Accordion
+const accSingleTriggers = document.querySelectorAll(".js-acc-single-trigger");
+
+accSingleTriggers.forEach(trigger =>
+  trigger.addEventListener("click", toggleAccordion)
+);
+
+function toggleAccordion() {
+  const items = document.querySelectorAll(".js-acc-item");
+  const thisItem = this.parentNode;
+
+  items.forEach(item => {
+    if (thisItem == item) {
+      thisItem.classList.toggle("is-open");
+      return;
+    }
+    item.classList.remove("is-open");
+  });
+}
